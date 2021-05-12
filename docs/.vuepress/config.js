13aa19cf96ba59eeaf27b717d20ef94fd2413a18
邹庚燏(Gengyu Zou) <zougengyu@genomics.cn>
2021-05-12 10:30:33 +0800

    init

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    title: 'Title',
+    description: 'desc',
+    displayAllHeaders: true,
+    themeConfig: {
+        logo: '/assets/img/logo.png',
+        nav: [
+            { text: 'Home', link: '/' },
+            { text: 'Java', link: '/java/' },
+            { text: 'External', link: 'https://google.com' },
+        ],
+        sidebar: 'auto'
+    }
+
+}
